67b9a63f6e5b5747c27a26ae17db93ac3e4026ed
Allison <asheren@neighborsations.com>
2013-10-01 12:29:17 -0400

    problem1 solution working. some tests still failing

--- lib/problem1/problem1.rb
+++ lib/problem1/problem1.rb
@@ -6,14 +6,13 @@ module Problem1
 
 	def self.multiples(stop_counting)
 
-		total = (1...stop_counting).find_all { |i| i%3 == 0 || i%5 ==0 }.reduce(:+)
+		(1...stop_counting).find_all { |i| i%3 == 0 || i%5 ==0 }.reduce(:+)
 		#total equals 1 up to stop_counting dot find_all, the block accepts an 
 		# argument i where i mod 5 is 0 or i mod 3 is 0 then chain 
   #   reduce and pass colon plus operator
 
-  #alternate solutions: Use .select or .map (pair reduce with map). write it with
-  #each and then rewrite with iterators
+  #alternate solutions: Use .select. write it with each and then rewrite with iterators
 		
 	end
-	puts total
+	#puts total
 end
