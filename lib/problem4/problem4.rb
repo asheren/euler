0313fb76a03b80c65131dac623a04ee2cd3f316d
Allison <asheren@neighborsations.com>
2013-10-03 14:07:53 -0400

    problem 4 code and tests

--- lib/problem4/problem4.rb
+++ lib/problem4/problem4.rb
@@ -3,4 +3,17 @@
 
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-#define palendrome, then turn numbers into a string and multiply all the numbers until you find a pal.
+#multiply all the numbers. then figure out which is a pal, then figure out max
+
+module Problem4
+
+max = 0
+(100..999).each do |a|
+	(a..999).each do |b|
+		product = a*b
+		max = [max, product].max if product.to_s == product.to_s.reverse
+	end
+end
+
+puts "#{max}"
+end
--- spec/problem4_spec.rb
+++ spec/problem4_spec.rb
@@ -1,8 +1,9 @@
 require 'problem4/problem4'
 
-describe 'answer' do 
+describe "largest of two digits" do 
+	it { product.max(10...99).should eq 9009 } 
+end
 
-	it { largest_palendrome_of_two_digits(10...100).should eq 9009 }
-	it { largest_palendrome_of_three_digits(100...1000).should eq __ }
-	
+describe "largest of three digits" do	
+	it { largest_palendrome_of_three_digits(100...1000).should eq 906609 } 
 end
